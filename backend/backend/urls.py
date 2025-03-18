44f835f283129fcc388abd534f7fa50e7c8750bb
Pranav P <pranavkrishnap856@gmail.com>
2025-03-18 09:25:45 +0530

    Added route for LoR Application page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lor/', views.lor_application_view, name='lor')
 ]
+
---
+++ backend/backend/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def lor_application_view(request):
+    return render(request, 'lor_application.html')
+
