2714804e938e1968b7f2be4b3c72f23cb9f1b5d7
Pranav P <pranavkrishnap856@gmail.com>
2025-03-24 13:29:14 +0530

    Reorganised files into relevant categoies and fixed routes for few pages

--- backend/higherstudiesportal/views.py
+++ backend/higherstudiesportal/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def lor_application_student(request):
-    return render(request, 'lor_application.html')
+    return render(request, 'student/lor_application.html')
 
 def dashboard_student(request):
-    return render(request, 'student_dashboard.html')
+    return render(request, 'student/student_dashboard.html')
 
 def dashboard_faculty(request):
-    return render(request, 'faculty_dashboard.html')
+    return render(request, 'faculty/faculty_dashboard.html')
 
 def letter_upload(request):
-    return render(request, 'cc1.html')
+    return render(request, 'student/cc1.html')
 
 def index(request):
     return render(request, 'index.html')
